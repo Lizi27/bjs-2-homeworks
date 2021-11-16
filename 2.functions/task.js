0fb77bc2215843cd89ac31208fb67983ec0aadba
Liza <lizi.zaitseva@gmail.com>
2021-11-16 21:37:54 +0300

    Work 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,59 @@
 // Задание 1
+"use strict";
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
+  let min, max, sum = 0, avg = 0;
+  min = Infinity;
+  max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    let el = arr[i];
+    if (el < min) {
+      min = el;
+    }
+    if (el > max) {
+      max = el;
+    }
+    sum += el;
+  }
+  avg = sum / arr.length;
+  avg = parseFloat(parseFloat(avg).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let el = arr[i];
+    sum += el;
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let el = func(arrOfArr[i]);
+    if (el > max) {
+      max = el;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    let el = arr[i];
+    if (el < min) {
+      min = el;
+    }
+    if (el > max) {
+      max = el;
+    }
+  }
+
+  return max - min;
 }
